06a079204c645080a7ca7e5d790981955e57bcf3
Artyom Kychin <artem.kychin@mail.ru>
2023-06-25 21:44:15 +0300

    All forms of SawgLabs are described. Only one Base Path Test added.

--- SwagLabs/Tests/BasePathTest.cs
+++ SwagLabs/Tests/BasePathTest.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SauceDemo.PageObject;
+
+namespace SauceDemo.Test
+{
+    internal class BasePathTest : SwagLabsBaseTest
+    {
+
+        [Test]
+        [Description("BasePathTest")]
+
+        public void GoBasePath()
+        {
+            var inventoryPage = new LoginPage(driver)
+                .OpenPage()
+                .LoginAsStandartUser()
+                .BuyProductAndGoToCart()
+                .GoToChekout()
+                .ContinueVsCustomerData()
+                .GoToFinish()
+                .BackHome();
+
+            Assert.IsNotNull(driver.FindElement(By.ClassName("shopping_cart_link")));
+        }
+    }
+}
