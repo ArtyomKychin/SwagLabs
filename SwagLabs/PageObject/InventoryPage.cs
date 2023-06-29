9b0aa17c949a390270312ff6c3daf1aa3daacb48
Artyom Kychin <artem.kychin@mail.ru>
2023-06-29 23:12:15 +0300

    Add CustomerDataTest & LogoutTest

--- SwagLabs/PageObject/InventoryPage.cs
+++ SwagLabs/PageObject/InventoryPage.cs
@@ -1,18 +1,21 @@
 ﻿using OpenQA.Selenium;
+using SauceDemo.PageObject;
 using SeleniumTests.SwagLabs.PageObject;
 
 namespace SwagLabs.PageObject
 {
-    internal class InventoryPage : BasePage  
+    internal class InventoryPage : BasePage
     {
         private By ShoppingCartLink = By.ClassName("shopping_cart_link");
         private By AddBikeLight = By.XPath("//*[@data-test='add-to-cart-sauce-labs-bike-light']");
-
+        private By BmMenuButton = By.ClassName("bm-burger-button");
+        private By LogoutButton = By.Id("logout_sidebar_link");      
+        
         public const string url = "https://www.saucedemo.com/inventory.html";
 
         public InventoryPage(WebDriver webDriver) : base(webDriver)
         {
-            WaitHelper.WaitElement(driver, ShoppingCartLink); 
+            WaitHelper.WaitElement(driver, ShoppingCartLink);
         }
 
         public override BasePage OpenPage()
@@ -27,5 +30,16 @@ namespace SwagLabs.PageObject
             driver.FindElement(ShoppingCartLink).Click();
             return new CartPage(driver);
         }
+
+        public InventoryPage GoToBmMenu()
+        {
+            driver.FindElement(BmMenuButton).Click();
+            return new InventoryPage(driver);
+        }
+
+        public void GoToLogout()
+        {
+            driver.FindElement(LogoutButton).Click();
+        }        
     }
 }
---
+++ SwagLabs/Tests/CustomerDataTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SauceDemo.PageObject;
+using SauceDemo.Test;
+
+
+namespace SwagLabs.Tests
+{
+    internal class CustomerDataTest : SwagLabsBaseTest
+    {
+        [Test]
+        [Description("CustomerDataTest")]
+
+        public void GoBasePath()
+        {
+            new LoginPage(driver)
+               .OpenPage()
+               .LoginAsStandartUser()
+               .BuyProductAndGoToCart()
+               .GoToChekout()
+               .ContinueWithoutCustomerData();
+
+            Assert.IsNotNull(driver.FindElement(By.XPath("//*[@data-test='error']")));
+        }
+    }
+}
